933d16d1f63be2d5ffcd3a6d44d2236a0377ff2b
Daniel Levin <sporks@gmail.com>
2016-05-18 11:47:59 -0700

    structured messages

--- chatCommands.js
+++ chatCommands.js
@@ -2,14 +2,61 @@ var token = "EAASI7Ir9omABAJjzbuKBY53QZB1POlZBfgFDtQrWGjQStusgSDvpELCZCWSl3ZCG6a
 var request = require('request');
 
 var chat = {};
-chat.sendTextMessage = sendTextMessage;
+// chat.sendTextMessage = sendTextMessage;
 
+chat.sendGenericMessage = function(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com/",
+            "title": "Web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        },{
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
-function sendTextMessage(sender, text) {
+chat.sendTextMessage = function(sender, text) {
   messageData = {
     text:text
   };
-
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {access_token:token},
