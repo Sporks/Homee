9118fcb25b4e15e73995f15a5b878d97cebb3716
Daniel Levin <sporks@gmail.com>
2016-05-18 12:37:32 -0700

    getting user id

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/webhook/', function(req, res) {
   res.send('Error, wrong validation token');
 });
 
-app.post('/webhook/', jsonParser, getInfo, function(req, res) {
+app.post('/webhook/', jsonParser, dbController.getInfo, function(req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
