23abf06da17915f7d9ffa4cafc3925bbac4f1317
Daniel Levin <sporks@gmail.com>
2016-05-18 11:03:14 -0700

    added validation token

--- index.js
+++ index.js
@@ -8,13 +8,35 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, './client')));
 
-app.get('/webhook/', function (req, res) {
+/******************************************
+ *******************************************
+ *******************************************
+ **************WEBHOOK STUFF****************
+ *******************************************
+ *******************************************
+ *******************************************/
+app.get('/webhook/', function(req, res) {
   if (req.query['hub.verify_token'] === 'this_is_my_token') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
-})
+});
 
-app.listen(process.env.PORT || 3000, function(){
+app.post('/webhook/', function(req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+      console.log(text);
+    }
+  }
+  res.sendStatus(200);
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
   console.log('Server is lisening on port 3000');
 });
