8ea8ff062f8554098952a0216ad314e5f9c47bd8
Daniel Levin <sporks@gmail.com>
2016-05-19 16:03:12 -0700

    chatbot stuck in a loop

--- index.js
+++ index.js
@@ -43,9 +43,10 @@ app.get('/webhook/', function(req, res) {
 
 app.post('/webhook/', jsonParser, function(req, res) {
   let messaging_events = req.body.entry[0].messaging;
+  req.info = {};
   for (let i = 0; i < messaging_events.length; i++) {
     let event = req.body.entry[0].messaging[i];
-    let sender = event.sender.id;
+    req.info.sender = event.sender.id;
     // req.sender = event.sender.id;
     if (event.message && event.message.text) {
       let text = event.message.text;
