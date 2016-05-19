74b3d22764e124aaf8a107d415e309aeab79acc1
Daniel Levin <sporks@gmail.com>
2016-05-19 13:50:50 -0700

    attempting to add new info to db

--- dbController.js
+++ dbController.js
@@ -4,53 +4,55 @@ const Update = require('./updateModel');
 
 module.exports = {
   getInfo: function(req, res, next){
-  let messaging_events = req.body.entry[0].messaging;
-    for (let i = 0; i < messaging_events.length; i++) {
-      let event = req.body.entry[0].messaging[i];
+    //Initialize req.info to store data
+    req.info = {};
+    let messaging_events = req.body.entry[0].messaging;
+      for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i];
 
-      //Save information in header for use with middleware.
-      req.info.sender = event.sender.id;
-      if (event.message && event.message.text) {
-        req.info.text = event.message.text;
-      }
-      var query = Update.where({user: req.info.sender, archived: false});
-      //Search for document we saved to continue asking questions;
-      query.findOne({},{},{ sort: { 'createdAt' : -1 } }, function(err, foundOne){
-        if(err){
-          console.log("ERROR ERROR     ", err);
+        //Save information in header for use with middleware.
+        req.info.sender = event.sender.id;
+        if (event.message && event.message.text) {
+          req.info.text = event.message.text;
         }
-        //IF we can't find the document, add it to the database and continue
-        if(!foundOne){
-          let created = Date.now();
-          let update = new Update({ user: req.sender,
-                                    createdAt: created,
-                                    questsAnsd: 0,
-                                    archived: false
-                                  });
-          //Save the document to the database
-          update.save( function(err, update){
-                  if(err) {
-                      console.error(err);
-                  }
-                  else {
-                      console.log (`${req.sender} - date:${created} has been added to the database.`);
-                  }
-          }).then(function(update){
-            //Add to header for use with questions
-            req.info.db = update;
+        var query = Update.where({user: req.info.sender, archived: false});
+        //Search for document we saved to continue asking questions;
+        query.findOne({},{},{ sort: { 'createdAt' : -1 } }, function(err, foundOne){
+          if(err){
+            console.log("ERROR ERROR     ", err);
+          }
+          //IF we can't find the document, add it to the database and continue
+          if(!foundOne){
+            let created = Date.now();
+            let update = new Update({ user: req.sender,
+                                      createdAt: created,
+                                      questsAnsd: 0,
+                                      archived: false
+                                    });
+            //Save the document to the database
+            update.save( function(err, update){
+                    if(err) {
+                        console.error(err);
+                    }
+                    else {
+                        console.log (`${req.sender} - date:${created} has been added to the database.`);
+                    }
+            }).then(function(update){
+              //Add to header for use with questions
+              req.info.db = update;
+              next();
+
+            });
+          }
+          //If we find it, add it to the header for use with questions
+          else if(foundOne){
+            console.log("SUP found", foundOne);
+            req.info.db = foundOne;
             next();
-
-          });
-        }
-        //If we find it, add it to the header for use with questions
-        else if(foundOne){
-          console.log("SUP found", foundOne);
-          req.info.db = foundOne;
-          next();
-        }
-      });
-    }
-  },
+          }
+        });
+      }
+    },
   updateInfo: function(info){
     var query = Update.where({user: info.sender, archived: false});
     query.findOneAndUpdate( info.db, function(err, newInfo){
