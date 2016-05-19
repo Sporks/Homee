0bf321149cf47b2339e295066ef604258fc7b588
Daniel Levin <sporks@gmail.com>
2016-05-19 12:45:23 -0700

    getting user id and checking length of message

--- dbController.js
+++ dbController.js
@@ -8,6 +8,15 @@ module.exports = {
       event = req.body.entry[0].messaging[i];
       // sender = event.sender.id;
       req.sender = event.sender.id;
+      let query = Update.where({user: req.sender});
+      query.findOne({},{},{ sort: { 'createdAt' : -1 } }, function(err, foundOne){
+        if(err){
+          console.log(err);
+        }
+        if(foundOne){
+          console.log(foundOne);
+        }
+      });
     }
     console.log(req.sender);
     next();
