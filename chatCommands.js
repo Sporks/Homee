e217331ca216db2ed2b8ee742affa232a42ba2f2
Daniel Levin <sporks@gmail.com>
2016-05-19 15:08:14 -0700

    adding questions and functionality

--- chatCommands.js
+++ chatCommands.js
@@ -6,92 +6,50 @@ var request = require('request');
 
 var chat = {};
 // chat.sendTextMessage = sendTextMessage;
+var questions ={  "q1": {"q": "Hello and welcome to Homee!  What room can we help you with?",
+                        "answers": ["Living Room", "Bedroom", "Office",
+                                    "Dining Room", "Outdoor"]},
+                  "q2": {"q": "What is your budget?",
+                        "answers": ["$500 and under", "­$500 ­- $1000","$1000 - $3000",
+                                    "$3000 - $5000","Over $5000"]},
+                  "q3": {"q": "When do you need your furniture by?",
+                        "answers": ["0 - 1 Weeks", "1-2 Weeks", "3-4 Weeks",
+                                    "1 Month or more"]},
+                  "q4": {"q": "How would you describe your style?",
+                        "answers": ["Modern", "Traditional", "Industrial",
+                                    "Eclectic", "Contemporary"]},
+                  "q5": {"q": "Can you send us some pictures of your space?"},
+                  "q6": {"q": "Do you have any special requests or additional information?"}
+                };
 
-chat.sendGenericMessage = function(sender) {
-  var messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "First card",
-          "subtitle": "Element #1 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com/",
-            "title": "Web url"
-          }, {
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for first element in a generic bubble",
-          }],
-        },{
-          "title": "Second card",
-          "subtitle": "Element #2 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-          "buttons": [{
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for second element in a generic bubble",
-          }],
-        }]
-      }
-    }
-  };
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+
+chat.questions = function(req, res){
+
 }
 
+
 chat.askQuestions = function(req, res, next){
-  var messageData = {
-    text: req.info.text,
-    db: req.info.db
-  };
+  switch(req.info.db.questsAnsd){
+    case 0:
+       chat.sendTextMessage(req.info.sender, questions.q1.q);
+       break;
+  }
+  // var messageData = {
+  //   text: req.info.text
+  // };
   //Promisify updating the database
   var p1 = new Promise((resolve, reject)=>{
     //REMEMBER TO RESOLVE PROMISES!
-      dbController.updateInfo(req.info, resolve, reject);
-      // console.log(resolve, reject);
+    dbController.updateInfo(req.info, resolve, reject);
+    // console.log(resolve, reject);
     });
   p1.then(function(val){
-    console.log(req.info.text, "DA       ");
-    request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token:token},
-      method: 'POST',
-      json: {
-        recipient: {id:req.info.sender},
-        message: messageData,
-      }
-    }, function(error, response, body) {
-      if (error) {
-        console.log('Error sending message: ', error);
-      } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-      }
-      // res.end();
-      next();
-    });
+
   }).catch((val)=>{
       console.log("Promise rejected: ", val);
       next();
   });
 }
-
 
 chat.sendTextMessage = function(sender, text) {
   var messageData = {
@@ -111,28 +69,6 @@ chat.sendTextMessage = function(sender, text) {
     } else if (response.body.error) {
       console.log('Error: ', response.body.error);
     }
-  });
-}
-
-chat.sendTextMessage2 = function(req, res, next) {
-  var messageData = {
-    text:req.info.text
-  };
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:req.info.sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-    next();
   });
 }
 
