5b7f345025041bcecd51bb2e4c1fccf8534140bd
Daniel Levin <sporks@gmail.com>
2016-05-19 13:08:52 -0700

    attempting to add to database

--- dbController.js
+++ dbController.js
@@ -13,13 +13,33 @@ module.exports = {
       query.findOne({},{},{ sort: { 'createdAt' : -1 } }, function(err, foundOne){
         if(err){
           console.log("ERROR ERROR     ", err);
+          next();
+        }
+        if(!foundOne){
+          console.log("NOT FOUND    ", foundOne);
+          let created = Date.now();
+          let update = new Update({ user: req.sender,
+                                    createdAt: created,
+                                    questsAnsd: 0,
+                                    archived: false
+                                  });
+          update.save( function(err, update){
+                  if(err) {
+                      console.error(err);
+                  }
+                  else {
+                      console.log (`${req.sender} - date:${created} has been added to the database.`);
+                      next();
+                  }
+          });
         }
         if(foundOne){
-          console.log("FOUND FOUND    ", foundOne);
+          console.log("SUP found");
+          next();
         }
       });
     }
     // console.log(req.sender);
-    next();
+    // next();
   }
 };
