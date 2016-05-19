a2b3858a6d36a716c909bb1b48e4504b778984e5
Daniel Levin <sporks@gmail.com>
2016-05-19 15:27:25 -0700

    adding questions and functionality

--- dbController.js
+++ dbController.js
@@ -19,40 +19,41 @@ module.exports = {
       }
       var query = Update.where({user: req.info.sender, archived: false});
       //Search for document we saved to continue asking questions;
-      query.findOne({},{},{ sort: { 'createdAt' : -1 } }, function(err, foundOne){
-        if(err){
-          console.log("ERROR ERROR     ", err);
-        }
-        //IF we can't find the document, add it to the database and continue
-        if(!foundOne){
-          let created = Date.now();
-          let update = new Update({ user: req.sender,
-                                    createdAt: created,
-                                    questsAnsd: 0,
-                                    archived: false
-                                  });
-          //Save the document to the database
-          update.save( function(err, update){
-                  if(err) {
-                      console.error(err);
-                  }
-                  else {
-                      console.log (`${req.sender} - date:${created} has been added to the database.`);
-                  }
-          }).then(function(update){
-            //Add to header for use with questions
-            req.info.db = update;
-            next();
-
-          });
-        }
-        //If we find it, add it to the header for use with questions
-        else if(foundOne){
-          console.log("SUP found");
-          req.info.db = foundOne;
-          next();
-        }
-      });
+      // query.findOne({},{},{ sort: { 'createdAt' : -1 } }, function(err, foundOne){
+      //   if(err){
+      //     console.log("ERROR ERROR     ", err);
+      //   }
+      //   //IF we can't find the document, add it to the database and continue
+      //   if(!foundOne){
+      //     let created = Date.now();
+      //     let update = new Update({ user: req.sender,
+      //                               createdAt: created,
+      //                               questsAnsd: 0,
+      //                               archived: false
+      //                             });
+      //     //Save the document to the database
+      //     update.save( function(err, update){
+      //             if(err) {
+      //                 console.error(err);
+      //             }
+      //             else {
+      //                 console.log (`${req.sender} - date:${created} has been added to the database.`);
+      //             }
+      //     }).then(function(update){
+      //       //Add to header for use with questions
+      //       req.info.db = update;
+      //       next();
+      //
+      //     });
+      //   }
+      //   //If we find it, add it to the header for use with questions
+      //   else if(foundOne){
+      //     console.log("SUP found");
+      //     req.info.db = foundOne;
+      //     next();
+      //   }
+      // });
+      next();
     }
     },
   updateInfo: function(info, resolve, reject){
