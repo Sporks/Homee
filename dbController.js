da6ef970d522faa24d61f95161c0d08670010cc1
Daniel Levin <sporks@gmail.com>
2016-05-19 13:14:56 -0700

    attempting to add to database

--- dbController.js
+++ dbController.js
@@ -7,7 +7,6 @@ module.exports = {
   let messaging_events = req.body.entry[0].messaging;
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i];
-      // sender = event.sender.id;
       req.sender = event.sender.id;
       var query = Update.where({user: req.sender});
       query.findOne({},{},{ sort: { 'createdAt' : -1 } }, function(err, foundOne){
@@ -33,7 +32,7 @@ module.exports = {
                   }
           });
         }
-        if(foundOne){
+        else if(foundOne){
           console.log("SUP found");
           next();
         }
